f298bf2d42eac4a01db8291f84fb9e5ceda5ad42
angelhpascual <angelhpascual@gmail.com>
2020-09-22 18:02:20 +0200

    Added React-flip-move FINAL

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,6 +3,7 @@ import "./Feed.css";
 import Post from "./Post";
 import TweetBox from "./TweetBox";
 import db from "../Firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -22,20 +23,21 @@ function Feed() {
 
         {/* TweetBox */}
         <TweetBox />
-
-        {/* Post */}
-        {posts.map((post) => (
-          <Post
-            key={post.text}
-            displayName={post.displayName}
-            username={post.username}
-            verified={post.verified}
-            text={post.text}
-            image={post.image}
-            avatar={post.avatar}
-            timestamp={post.timestamp}
-          />
-        ))}
+        <FlipMove>
+          {/* Post */}
+          {posts.map((post) => (
+            <Post
+              key={post.text}
+              displayName={post.displayName}
+              username={post.username}
+              verified={post.verified}
+              text={post.text}
+              image={post.image}
+              avatar={post.avatar}
+              timestamp={post.timestamp}
+            />
+          ))}
+        </FlipMove>
       </div>
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -4,52 +4,49 @@ import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
-import React from "react";
+import React, { forwardRef } from "react";
 import "./Post.css";
 
-function Post({
-  displayName,
-  username,
-  verified,
-  text,
-  image,
-  avatar,
-  timestamp,
-}) {
-  return (
-    <div className="post">
-      <div className="post__avatar">
-        <Avatar src={avatar} />
-      </div>
+const Post = forwardRef(
+  (
+    { displayName, username, verified, text, image, avatar, timestamp },
+    ref
+  ) => {
+    return (
+      <div className="post" ref={ref}>
+        <div className="post__avatar">
+          <Avatar src={avatar} />
+        </div>
 
-      <div className="post__body">
-        <div className="post__header">
-          <div className="post__headerText">
-            <h3>
-              {displayName}{" "}
-              <span className="post__headerSpecial">
-                {verified && <VerifiedUserIcon className="post__badge" />}@
-                {username}
-                <span className="post__timestamp">
-                  &nbsp; · &nbsp;{new Date(timestamp?.toDate()).toUTCString()}
+        <div className="post__body">
+          <div className="post__header">
+            <div className="post__headerText">
+              <h3>
+                {displayName}{" "}
+                <span className="post__headerSpecial">
+                  {verified && <VerifiedUserIcon className="post__badge" />}@
+                  {username}
+                  <span className="post__timestamp">
+                    &nbsp; · &nbsp;{new Date(timestamp?.toDate()).toUTCString()}
+                  </span>
                 </span>
-              </span>
-            </h3>
+              </h3>
+            </div>
+            <div className="post__headerDescription">
+              <p>{text}</p>
+            </div>
           </div>
-          <div className="post__headerDescription">
-            <p>{text}</p>
+          <img src={image} alt="GIF" />
+          <div className="post__footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
           </div>
-        </div>
-        <img src={image} alt="GIF" />
-        <div className="post__footer">
-          <ChatBubbleOutlineIcon fontSize="small" />
-          <RepeatIcon fontSize="small" />
-          <FavoriteBorderIcon fontSize="small" />
-          <PublishIcon fontSize="small" />
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default Post;
